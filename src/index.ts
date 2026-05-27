28ebd3d0b35b8ddb7f242216da68b04e41371196
Shobhit Sharma <shobhit.sharma@mrikal.com>
2026-05-27 12:15:44 +0530

    cd workflow added

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import express from "express";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 8080;
 
 app.get("/", (req, res) => {
     res.send("Hello from TypeScript server 🚀");
 });
+
+app.get("/order", (req, res) => {
+  res.send("This is order Page");
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
